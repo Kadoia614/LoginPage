d1dc3874389756b7de2e1d8292cf887bec5f1548
Miguel Moraes <81337329+Kadoia614@users.noreply.github.com>
2023-08-17 17:15:22 -0300

    Main archives
    just main archives

---
+++ login.js
@@ -0,0 +1,7 @@
+function toggleForm(){
+    document.getElementById('login').classList.toggle('show');
+    document.getElementById('register').classList.toggle('show');
+    document.getElementById('login').classList.toggle('hiden');    
+    document.getElementById('register').classList.toggle('hiden');    
+}
+
